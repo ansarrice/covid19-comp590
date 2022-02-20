08ba8b3a4d88b91e2b05a19326b1887b8c4e3383
Ansar Ainiwaer <aa171@rice.edu>
2022-02-19 18:23:16 -0600

    ansar-change

---
+++ app/src/main/java/com/covid19/comp590/CounrtyData.java
@@ -0,0 +1,11 @@
+package com.covid19.comp590;
+
+public class CounrtyData {
+
+    public static  final String[] countryNames={
+            "IND","USA","MY","AFG","MF"
+    };
+    public  static final  int[] countryFlag ={
+            R.drawable.india,R.drawable.uk,R.drawable.malesiya,R.drawable.afganistan,R.drawable.maxico
+    };
+}

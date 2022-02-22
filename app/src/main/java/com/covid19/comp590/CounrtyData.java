85bdd5a74d760b30340eaf892c58062f82d12d9d
Ansar Ainiwaer <aa171@rice.edu>
2022-02-22 12:55:13 -0600

    second time

--- app/src/main/java/com/covid19/comp590/CounrtyData.java
+++ app/src/main/java/com/covid19/comp590/CounrtyData.java
@@ -3,9 +3,9 @@ package com.covid19.comp590;
 public class CounrtyData {
 
     public static  final String[] countryNames={
-            "IND","USA","MY","AFG","MF"
+            "USA","MY"
     };
     public  static final  int[] countryFlag ={
-            R.drawable.india,R.drawable.uk,R.drawable.malesiya,R.drawable.afganistan,R.drawable.maxico
+            R.drawable.usa,R.drawable.maxico
     };
 }
